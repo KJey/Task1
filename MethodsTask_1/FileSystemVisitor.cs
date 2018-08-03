0b4cfbc589d44ab1bce04cb4d211b97b0b62a749
KJey <35629622+KJey@users.noreply.github.com>
2018-08-03 10:30:07 +0600

    first commit

---
+++ MethodsTask_1/FileSystemVisitor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MethodsTask_1
+{
+    class FileSystemVisitor
+    {
+
+         private readonly DirectoryInfo DefaultPath;
+
+
+        public FileSystemVisitor(DirectoryInfo startDirectory)
+
+        {
+
+            DefaultPath = startDirectory;
+
+        }
+
+        public IEnumerable<FileSystemInfo> ShowFileSystemInfo()
+         {
+             foreach(var FileSystemInfo in ShowAllInside(DefaultPath))
+             {
+                yield return FileSystemInfo;
+               
+             }
+         }
+
+
+         public IEnumerable<FileSystemInfo> ShowAllInside(DirectoryInfo dirInf)
+         {
+             foreach (var FileSystemInfo in dirInf.EnumerateFileSystemInfos())
+             {
+                
+
+                if (FileSystemInfo is FileSystemInfo)
+
+                {
+
+                    yield return FileSystemInfo;
+                    
+
+                }
+
+
+                
+
+                if (FileSystemInfo is DirectoryInfo)
+                 {
+                     
+                     DirectoryInfo dir = new DirectoryInfo(FileSystemInfo.FullName.ToString());
+
+                    foreach (var InsideFolderInfo in ShowAllInside(dir))
+                     {
+                         yield return InsideFolderInfo;
+                     }
+                     continue;
+                 }
+                 
+                 
+             }
+         }
+
+
+    }
+}
---
+++ MethodsTask_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using MethodsTask_1;
+
+namespace MethodsTask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DirectoryInfo DefaultPath = new DirectoryInfo(@"D:\\12");
+
+            Console.WriteLine("Folder "+DefaultPath.FullName.ToString()+" contains:");
+
+            FileSystemVisitor FSV = new FileSystemVisitor(DefaultPath);
+
+            //var exit = FSV.ShowAllInside(DefaultPath); //ToString();
+            //Console.WriteLine(exit);
+            
+            foreach (var fileinfo in FSV.ShowFileSystemInfo())
+            {
+                
+                Console.WriteLine(fileinfo.FullName);
+            }
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
